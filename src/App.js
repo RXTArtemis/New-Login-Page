f88767b4d28b156dfcbca1e7acd92244ab7430af
RXTArtemis <r_nava20@outlook.com>
2023-03-15 11:42:23 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import StarwarsBtn from './components/starwars-btn';
+import NewNavbar from './components/newNavbar';
+import NewLoginForm from './components/newLoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="background">
+     <NewNavbar />
+     <br/>
+     <NewLoginForm />
+      <br/>
+      <StarwarsBtn />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/starwars-btn.js
@@ -0,0 +1,14 @@
+import "./starwars-btn.css";
+
+function StarwarsBtn() {
+  return (
+    <div class="container">
+    <div class="btn"><a href="#">Join the Darkside</a></div>
+    <div class="btn"><a href="#" >Log In</a></div>
+    <div class="btn"><a href="#" >Join the Lightside</a></div>
+
+</div>	
+  );
+}
+
+export default StarwarsBtn;
